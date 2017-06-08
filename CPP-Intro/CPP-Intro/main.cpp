b3a2d51e31add6f02c905754984e784fdf8fa52a
Keegan Jebb <ksjebb@gmail.com>
2017-06-08 12:02:28 -0400

    Initialized Xcode project and main.cpp

---
+++ CPP-Intro/CPP-Intro/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP-Intro
+//
+//  Created by Keegan Jebb on 2017-06-08.
+//  Copyright © 2017 Keegan Jebb. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
